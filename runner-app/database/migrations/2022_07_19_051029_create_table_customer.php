1f2f49508f7f8c1d074bbf526f047c48e4182544
ThuongBong <yenpham2114@gmail.com>
2022-07-19 12:42:19 +0700

    commit code

---
+++ runner-app/database/migrations/2022_07_19_051029_create_table_customer.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCustomer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id("idCustomer");
+            $table->string("name",50);
+            $table->string("phone",15);
+            $table->string("email",50);
+            $table->string("address",255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
